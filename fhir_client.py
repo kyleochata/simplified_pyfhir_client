eb0028bc3c34bd27a2792aee30d5debf29bd716e
kyleochata <contact.kyle.e_swe@proton.me>
2026-05-09 21:54:18 -0700

    added python-dotenv

--- fhir_client.py
+++ fhir_client.py
@@ -5,4 +5,11 @@ from typing import Any, Dict, List, Optional
 
 import requests
 
- 
+# ---------------------------------------------------------------------
+# Configuration
+# ---------------------------------------------------------------------
+
+FHIR_BASE_URL = "http://127.0.0.1:csp/healthshare/demo/fhir/r4"
+
+USERNAME = "_System"
+PASSWORD = "ISCDEMO"
